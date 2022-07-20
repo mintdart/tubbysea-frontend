65adc8bf5cf72ea435c81378dc6f0011f3e9a25f
mintdart <96025197+mintdart@users.noreply.github.com>
2022-07-20 02:00:44 -0700

    change logo size

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -26,7 +26,7 @@ export default function Layout({ children, ...props }: ILayoutProps) {
 			<header className={styles.header}>
 				<nav className={styles.nav}>
 					<div className={styles.logoWrapper}>
-						<Image src="/tubby.png" alt="tubby cat" layout="fill" />
+						<Image src="/tubby.png" alt="tubby cat" height="24px" width="22" objectFit="contain" priority />
 					</div>
 					<Link href="/">
 						<a data-active={router.pathname === '/'}>Borrow</a>
