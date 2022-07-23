89c2691cbd01944953063f307b0795d147d4777a
mintdart <96025197+mintdart@users.noreply.github.com>
2022-07-23 04:04:03 -0700

    refactor

--- components/Cart/index.tsx
+++ components/Cart/index.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { DisclosureState } from 'ariakit'
-import { Dialog, DialogDismiss, DialogHeading } from 'ariakit/dialog'
+import { Dialog, DialogHeading } from 'ariakit/dialog'
 import { useGetCartItems, useSaveItemToCart } from '~/hooks/useCart'
 import { useGetQuote } from '~/hooks/useGetQuote'
 import styles from './Cart.module.css'
@@ -21,7 +21,21 @@ export default function Cart({ dialog }: { dialog: DisclosureState }) {
 			<header className={styles.dialogHeader}>
 				<DialogHeading className={styles.dialogHeading}>Checkout</DialogHeading>
 				<Link href="/">
-					<DialogDismiss as="a" className={styles.dialogButtonDismiss} />
+					<a className={styles.dialogButtonDismiss}>
+						<svg
+							aria-hidden="true"
+							fill="none"
+							height="10"
+							viewBox="0 0 10 10"
+							width="10"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M1.70711 0.292893C1.31658 -0.0976311 0.683417 -0.0976311 0.292893 0.292893C-0.0976311 0.683417 -0.0976311 1.31658 0.292893 1.70711L3.58579 5L0.292893 8.29289C-0.0976311 8.68342 -0.0976311 9.31658 0.292893 9.70711C0.683417 10.0976 1.31658 10.0976 1.70711 9.70711L5 6.41421L8.29289 9.70711C8.68342 10.0976 9.31658 10.0976 9.70711 9.70711C10.0976 9.31658 10.0976 8.68342 9.70711 8.29289L6.41421 5L9.70711 1.70711C10.0976 1.31658 10.0976 0.683417 9.70711 0.292893C9.31658 -0.0976311 8.68342 -0.0976311 8.29289 0.292893L5 3.58579L1.70711 0.292893Z"
+								fill="currentColor"
+							></path>
+						</svg>
+					</a>
 				</Link>
 			</header>
 
