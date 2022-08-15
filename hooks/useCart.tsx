56178f061ee76c7113392aa3a66bee4fca29e526
mintdart <96025197+mintdart@users.noreply.github.com>
2022-08-15 22:15:31 +0000

    refactor cart queries

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -6,53 +6,61 @@ import { ICart, IError, INftItem } from './types'
 import { useGetNftsList } from './useNftsList'
 
 // save/remove items from local storage
-function saveItemToCart({ chainId, tokenId }: { chainId?: number; tokenId: number }) {
-	const contract = chainId ? chainConfig[chainId]?.borrowNftAddress : null
+async function saveItemToCart({ chainId, tokenId }: { chainId?: number; tokenId: number }) {
+	try {
+		const contract = chainId ? chainConfig[chainId]?.borrowNftAddress : null
 
-	if (!contract) {
-		throw new Error("Error: Couldn't get contract address of nft collection")
-	}
-
-	const prevItems = localStorage.getItem(LOCAL_STORAGE_KEY)
-
-	if (prevItems) {
-		const items = JSON.parse(prevItems)
-
-		const contractItems: Array<number> = items[contract] || []
-
-		if (contractItems.includes(tokenId)) {
-			items[contract] = contractItems.filter((item) => item !== tokenId)
-		} else {
-			items[contract] = [...contractItems, tokenId]
+		if (!contract) {
+			throw new Error("Error: Couldn't get contract address of nft collection")
 		}
 
-		localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(items))
-	} else {
-		localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify({ [contract]: [tokenId] }))
-	}
+		const prevItems = localStorage.getItem(LOCAL_STORAGE_KEY)
 
-	return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}')
+		if (prevItems) {
+			const items = JSON.parse(prevItems)
+
+			const contractItems: Array<number> = items[contract] || []
+
+			if (contractItems.includes(tokenId)) {
+				items[contract] = contractItems.filter((item) => item !== tokenId)
+			} else {
+				items[contract] = [...contractItems, tokenId]
+			}
+
+			localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(items))
+		} else {
+			localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify({ [contract]: [tokenId] }))
+		}
+
+		return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}')
+	} catch (error: any) {
+		throw new Error("Couldn't add item to cart")
+	}
 }
 
 // get cart items from local storage
-function fetchCartItems({ chainId, tubbies }: { chainId?: number; tubbies?: Array<INftItem> }) {
-	const contract = chainId ? chainConfig[chainId]?.borrowNftAddress : null
+async function fetchCartItems({ chainId, tubbies }: { chainId?: number; tubbies?: Array<INftItem> }) {
+	try {
+		const contract = chainId ? chainConfig[chainId]?.borrowNftAddress : null
 
-	if (!contract) {
-		throw new Error("Error: Couldn't get contract address of nft collection")
+		if (!contract) {
+			throw new Error("Error: Couldn't get contract address of nft collection")
+		}
+
+		const prevItems = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}')
+
+		const itemsInStorage: Array<number> = prevItems[contract] || []
+
+		return itemsInStorage
+			.map((item) => {
+				const tubby = tubbies?.find((t) => t.tokenId === item)
+
+				return tubby || null
+			})
+			.filter((item) => item) as Array<INftItem>
+	} catch (error: any) {
+		throw new Error("Couldn't get items in cart")
 	}
-
-	const prevItems = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}')
-
-	const itemsInStorage: Array<number> = prevItems[contract] || []
-
-	return itemsInStorage
-		.map((item) => {
-			const tubby = tubbies?.find((t) => t.tokenId === item)
-
-			return tubby || null
-		})
-		.filter((item) => item) as Array<INftItem>
 }
 
 // *------------------------------------------------*
@@ -70,9 +78,8 @@ const useSaveItemToCart = () => {
 
 			const contractAddress: string | undefined | null = chain?.id ? chainConfig[chain.id]?.borrowNftAddress : null
 
-			return contractAddress ? cart[contractAddress] : []
+			return contractAddress ? cart[contractAddress] || [] : []
 		},
-
 		onSuccess: (data: ICart, _variables, prevItems) => {
 			const contractAddress: string | undefined | null = chain?.id ? chainConfig[chain.id]?.borrowNftAddress : null
 
@@ -81,7 +88,6 @@ const useSaveItemToCart = () => {
 				router.push('/?cart=true')
 			}
 		},
-
 		onSettled: () => {
 			queryClient.invalidateQueries()
 		}
