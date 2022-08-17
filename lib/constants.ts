83d4b854fbaab3a59fe9f325ab4553d722b9377f
mintdart <96025197+mintdart@users.noreply.github.com>
2022-08-17 06:20:13 +0000

    update nft api endpoints

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,7 +6,7 @@ export const chainConfig: IChainConfig = {
 	1: {
 		ankrUrl: 'https://rpc.ankr.com/eth',
 		alchemyUrl: 'https://eth-mainnet.g.alchemy.com/v2/5uLJQgmJyFsgKvbnnnZHuPLGtgzdSSF_',
-		alchemyNftUrl: 'https://eth-mainnet.alchemyapi.io/nft/v2/5uLJQgmJyFsgKvbnnnZHuPLGtgzdSSF_/getNFTs/',
+		alchemyNftUrl: 'https://eth-mainnet.g.alchemy.com/nft/v2/5uLJQgmJyFsgKvbnnnZHuPLGtgzdSSF_/getNFTs',
 		infuraUrl: 'https://mainnet.infura.io/v3/d24592b20f8b44a5a932dfb3c095d03a',
 		borrowNftAddress: TUBBY_CATS_ADDRESS,
 		repayNftAddress: LENDING_POOL_ADDRESS,
@@ -15,7 +15,7 @@ export const chainConfig: IChainConfig = {
 	5: {
 		ankrUrl: 'https://rpc.ankr.com/eth_goerli',
 		alchemyUrl: 'https://eth-goerli.g.alchemy.com/v2/5uLJQgmJyFsgKvbnnnZHuPLGtgzdSSF_',
-		alchemyNftUrl: 'https://eth-goerli.alchemyapi.io/nft/v2/5uLJQgmJyFsgKvbnnnZHuPLGtgzdSSF_/getNFTs/',
+		alchemyNftUrl: 'https://eth-goerli.g.alchemy.com/nft/v2/5uLJQgmJyFsgKvbnnnZHuPLGtgzdSSF_/getNFTs',
 		infuraUrl: 'https://goerli.infura.io/v3/d24592b20f8b44a5a932dfb3c095d03a',
 		borrowNftAddress: NFT_TESTNET_ADDRESS,
 		repayNftAddress: LENDING_POOL_ADDRESS,
