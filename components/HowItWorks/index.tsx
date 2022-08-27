ec43bc5ce12a2fc6a34f6a6053bee0e042b9825f
mintdart <96025197+mintdart@users.noreply.github.com>
2022-08-27 05:37:27 -0400

    add current annual interest on landing page

--- components/HowItWorks/index.tsx
+++ components/HowItWorks/index.tsx
@@ -2,12 +2,16 @@ import { useRouter } from 'next/router'
 import { useChainModal, useConnectModal } from '@rainbow-me/rainbowkit'
 import { useAccount } from 'wagmi'
 import styles from './HowItWorks.module.css'
+import { useGetInterest } from '~/hooks/useInterest'
 
 export default function HowItWorks() {
 	const router = useRouter()
 	const { isConnected } = useAccount()
 	const { openConnectModal } = useConnectModal()
 	const { openChainModal } = useChainModal()
+	const { data } = useGetInterest()
+
+	const currentAnnualInterest = typeof data === 'string' && !Number.isNaN(Number(data)) ? Number(data) : null
 
 	return (
 		<article className={styles.wrapper}>
@@ -17,7 +21,21 @@ export default function HowItWorks() {
 				<li className={styles.listItem}>Deposit tubbies as collateral and borrow ETH</li>
 				<li className={styles.listItem}>All loans last for 2 weeks max and there are no liquidations</li>
 				<li className={styles.listItem}>Only pay for the time you borrow</li>
-				<li className={styles.listItem}>Interest is based on pool utilization and capped at 80%</li>
+				<li className={styles.listItem}>
+					<span>Interest rate is </span>
+					{currentAnnualInterest ? (
+						<span>{`${currentAnnualInterest}%`}</span>
+					) : (
+						<span className={`placeholder-container ${styles.interestLoading}`}></span>
+					)}
+					<span> per annum, and over 2 week loan term that equals </span>
+					{currentAnnualInterest ? (
+						<span>{`${(currentAnnualInterest / 14).toFixed(2)}%`}</span>
+					) : (
+						<span className={`placeholder-container ${styles.interestLoading}`}></span>
+					)}
+					<span> interest for the duration of the loan</span>
+				</li>
 			</ul>
 
 			{isConnected ? (
